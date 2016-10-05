00f4729b705c6ded0db0b00a57460ffe6ff76dcb
Shou Xian <renegod9@gmail.com>
2016-10-05 22:33:39 +0800

    Changed some code to produce output.txt file
    Output.txt now has 1000 posts inside of it. Please do not overwrite this file

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,71 @@
 import org.apache.commons.compress.archivers.sevenz.SevenZArchiveEntry;
 import org.apache.commons.compress.archivers.sevenz.SevenZFile;
+import org.apache.commons.io.output.ByteArrayOutputStream;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Calendar;
+import java.util.stream.Stream;
 
 public class Main {
+
     public static void main(String[] args) {
         SevenZFile sevenZFile;
         try {
             // change this file to the 7z file which you have downloaded
-            sevenZFile = new SevenZFile(new File("/Users/shouxian/Downloads/stackoverflow.com-PostLinks.7z"));
+            File zipFile = new File("/Users/shouxian/Downloads/stackoverflow.com-Posts.7z");
+
+            Path f = Paths.get("/Users/shouxian/Downloads/stackoverflow.com-Posts.7z");
+            sevenZFile = new SevenZFile(zipFile);
 
             // the files inside of the SevenZFile object acts like a list
             // getNexEntry() will get the next item inside of the list
             SevenZArchiveEntry entry = sevenZFile.getNextEntry();
             while(entry!=null){
-                // prints out the name of the file and the size of the file in bytes
-                System.out.println(entry.getName() + " (Size: " + entry.getSize() + ")");
+                String name = entry.getName();
 
-                // place the file name into an outputstream object
-                FileOutputStream out = new FileOutputStream(entry.getName());
-                byte[] content = new byte[(int) entry.getSize()];
+                if(entry.isDirectory()) {
+                    System.out.println(String.format("Found directory entry %s", name));
+                } else {
+                    // If this is a file, we read the file content into a
+                    // ByteArrayOutputStream ...
+                    System.out.println(String.format("Unpacking %s ...", name));
+                    ByteArrayOutputStream contentBytes = new ByteArrayOutputStream();
 
-                // uses the outputstream object to write into the byte array object above
-                sevenZFile.read(content, 0, content.length);
-                out.write(content);
-                out.close();
+                    // ... using a small buffer byte array.
+                    byte[] buffer = new byte[2048];
 
-                // and a BufferedReader on top to comfortably read the file
-                BufferedReader reader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(content)));
-                String line;
+                    //Java 8: Stream class
+                    Stream<String> lines = Files.lines(f, StandardCharsets.UTF_8 );
 
-                // this while loop will read the file line by line until it reaches the EOF line
-                while ((line = reader.readLine()) != null)
-                {
-                    // prints out the line in the file
-                    // we can do other stuff to the line string
-                    System.out.println(line);
+                    int bytesRead;
+
+                    Calendar cal = Calendar.getInstance();
+
+                    long startTime = cal.getTimeInMillis();
+                    long currentTime =startTime;
+
+                    FileWriter fw = new FileWriter("output.txt", true);
+                    BufferedWriter bw = new BufferedWriter(fw);
+                    PrintWriter out = new PrintWriter(bw);
+
+                    while(currentTime<startTime+1000 && (bytesRead = sevenZFile.read(buffer)) != -1) {
+                        startTime = cal.getTimeInMillis();
+                        contentBytes.write(buffer, 0, bytesRead);
+                        // Assuming the content is a UTF-8 text file we can interpret the
+                        // bytes as a string.
+                        String content = contentBytes.toString("UTF-8");
+                        // System.out.println(content);
+                        out.println(content);
+                    }
+
+                    out.close();
                 }
+
+                System.out.println("Done");
 
                 // gets the next item inside of the zip file
                 entry = sevenZFile.getNextEntry();
@@ -46,6 +75,8 @@ public class Main {
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
+        } finally {
+
         }
     }
 }
